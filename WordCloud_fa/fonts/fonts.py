7c59c074e5548870414a42f5f0f9334284e46f26
reza <parvvaresh@gmail.com>
2024-09-12 22:45:49 +0330

    add Fonts

---
+++ WordCloud_fa/fonts/fonts.py
@@ -0,0 +1,18 @@
+import json
+
+
+class Fonts:
+    def __init__(self) -> None:
+        
+        with open("info.json", "r") as file:
+            self.info_fonts = json.load(file)
+        print(self.info_fonts)
+        
+    def SetFont(self, name : str) -> str:
+        if name not in self.info_fonts:
+            raise ValueError(f"The font '{name}' does not exist.")
+        else:
+            return self.info_fonts[name]
+    
+    def getInfo(self) -> list:
+        return list(self.info_fonts.keys())
