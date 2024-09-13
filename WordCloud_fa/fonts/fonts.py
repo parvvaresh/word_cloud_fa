4dd4d4fd0e80393d55567b81af7b8df85fd0fb5c
reza <parvvaresh@gmail.com>
2024-09-13 23:24:30 +0330

    fix path from fonts

--- WordCloud_fa/fonts/fonts.py
+++ WordCloud_fa/fonts/fonts.py
@@ -4,9 +4,8 @@ import json
 class Fonts:
     def __init__(self) -> None:
         
-        with open("info.json", "r") as file:
+        with open("./fonts/info.json", "r") as file:
             self.info_fonts = json.load(file)
-        print(self.info_fonts)
         
     def SetFont(self, name : str) -> str:
         if name not in self.info_fonts:
